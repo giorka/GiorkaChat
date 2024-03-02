832e3c925fe4daf2b2bcc451b2e414e910bd6097
giorka <mokenosov@ro.ru>
2024-03-02 14:21:14 +0300

    stable version 0.1

--- chat/consumers.py
+++ chat/consumers.py
@@ -1,9 +1,9 @@
-from json import loads
-from typing import NoReturn
+from json import loads, dumps
+from typing import NoReturn, Optional
 
 from channels.generic.websocket import AsyncWebsocketConsumer
 from channels.layers import get_channel_layer
-from redis import Redis
+from redis import asyncio as aioredis
 
 
 class ConsumerMixin:
@@ -19,64 +19,94 @@ class Consumer(AsyncWebsocketConsumer, ConsumerMixin):
     TODO: определить метод для поиска
     """
 
-    redis = Redis(db=2)
+    redis = aioredis.Redis()
     channel_layer = get_channel_layer()
 
     class Meta:
         KEY = 'query'
         CLEAR_TIME = 43200  # NOTE: in seconds
 
-    @classmethod
-    def remove(cls, value: str) -> NoReturn:
-        cls.redis.lrem(name=cls.Meta.KEY, count=1, value=value)
+    def __init__(self, *args, **kwargs):
+        super().__init__(args, kwargs)
+        self.group_name: Optional[str] = None
 
-    async def disconnect(self, code) -> NoReturn:
+    @classmethod
+    async def remove(cls, value: str) -> NoReturn:
+        await cls.redis.lrem(name=cls.Meta.KEY, count=1, value=value)
+
+    async def disconnect(self, code: str) -> NoReturn:
         """
         TODO: отправить письмо собеседнику (если есть), что пользователь покинул чат
         """
 
-        self.remove(value=self.channel_name)
+        await self.remove(value=self.channel_name)
 
-    async def receive(self, text_data=None, bytes_data=None) -> NoReturn:
-        data = loads(s=text_data) | self.__dict__
+    async def receive(self, text_data: str = None, bytes_data: bytes = None) -> NoReturn:
+        data: dict = loads(s=text_data) | self.__dict__
 
-        message_type = data.get('type')
+        message_type: str = data.get('type')
 
         match message_type:
             case 'search':
                 await self.search(data=data)
-            # case '':
-            #     ...
+            case 'message':
+                await self.send_message(data=data)
 
-    @classmethod
-    async def join_group(cls, target: str, seeker: str) -> NoReturn:
-        group_name = str(hash((target.strip('specific.') + seeker.strip('specific.'))))
+    async def join_group(self, target: str, seeker: str) -> NoReturn:
+        self.group_name: str = str(hash((target.strip('specific.'))))
 
         for channel_name in (target, seeker):
-            await cls.channel_layer.group_add(
-                group=group_name,
+            await self.channel_layer.group_add(
+                group=self.group_name,
                 channel=channel_name,
 
             )
 
-        cls.remove(value=target)
+        await self.remove(value=target)
 
     @classmethod
-    def search(cls, data: dict) -> NoReturn:
-        channel_names = cls.redis.lrange(name=cls.Meta.KEY, start=0, end=0)
-        channel_name = (
-            channel_names[0].decode('UTF-8') if channel_names else None
-        )  # name of the channel the user we are connecting to
+    async def add_to_query(cls, *values: tuple) -> NoReturn:
+        """
+        FIXME: добавление expires
+        """
 
+        pipeline = cls.redis.pipeline()
+
+        if (await cls.redis.ttl(cls.Meta.KEY)) == -1:
+            await pipeline.expire(cls.Meta.KEY, time=cls.Meta.CLEAR_TIME)
+
+        await pipeline.rpush(cls.Meta.KEY, *values)
+        await pipeline.execute()
+
+    async def search(self, data: dict) -> None:
+        channel_names = await self.redis.lrange(name=self.Meta.KEY, start=0, end=0)
+        channel_name = (channel_names[0].decode('UTF-8') if channel_names else None)
         user_channel_name = data.get('channel_name')  # channel name of the user who is connecting
-
-        # TODO: refactor
+r
         if channel_name:
-            return cls.join_group(target=channel_name, seeker=user_channel_name)
-        else:
-            with cls.redis.pipeline() as pipeline:
-                if cls.redis.ttl(cls.Meta.KEY) == -1:
-                    pipeline.expire(cls.Meta.KEY, time=cls.Meta.CLEAR_TIME)
+            return await self.join_group(target=channel_name, seeker=user_channel_name)
 
-                pipeline.rpush(cls.Meta.KEY, user_channel_name)
-                pipeline.execute()
+        await self.add_to_query(user_channel_name)
+
+    async def send_message(self, data: dict) -> NoReturn:
+        group_name = (self.group_name if self.group_name else str(hash(self.channel_name.strip('specific.'))))
+
+        await self.channel_layer.group_send(
+            group_name,
+            {
+                'type': 'message',
+                'message': data.get('message'),
+            }
+        )
+
+    async def message(self, event):
+        message = event['message']
+
+        await self.send(
+            text_data=dumps(
+                dict(
+                    message=message,
+
+                )
+            )
+        )
